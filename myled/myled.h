845cb39d6839d4533f9306ab3a74e55d1451d81b
GemisonAlves <46090294+GemisonAlves@users.noreply.github.com>
2019-08-17 09:04:46 -0300

    Add files via upload

---
+++ myled/myled.h
@@ -0,0 +1,20 @@
+#ifndef MYLED_H
+#define MYLED_H
+
+#include "Arduino.h"
+class MyLed{
+	private:
+  		int pino;
+  	public:
+  		MyLed(int p);
+  		void acender();
+  		void apagar();
+  		void blink(int intervalo);
+  		void dimmer(int valor);
+  		void fadeIn(int intervalo);
+  		void fadeOut(int intervalo);
+                int getPino();
+                bool status();
+};
+
+#endif
